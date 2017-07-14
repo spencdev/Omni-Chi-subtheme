bacb9823ecc3c63a8d781cbe45f4660733c9fa3d
spencerlol1 <stwiehaus123@gmail.com>
2017-07-14 09:29:53 -0400

    initial commit

---
+++ sites/default/files/php/twig/5967c8e13add3_block--page-title-block.h_m58ED-f5qbaaNT4iDDIOPgo5q/G8BHXRe6a7WcxLZGle5CdADEFgWYfp4Yt0eg251sfZ4.php
@@ -0,0 +1,78 @@
+<?php
+
+/* themes/bootstrap 7/templates/block/block--page-title-block.html.twig */
+class __TwigTemplate_32955fa1d5c5fe9efe497f29680f24a5269d195b0236cc332b993f61eadf9fc9 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 9
+        $this->parent = $this->loadTemplate("block--bare.html.twig", "themes/bootstrap 7/templates/block/block--page-title-block.html.twig", 9);
+        $this->blocks = array(
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "block--bare.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/bootstrap 7/templates/block/block--page-title-block.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  11 => 9,);
+    }
+
+    public function getSource()
+    {
+        return "{#
+/**
+ * @file
+ * Theme override for page title block.
+ *
+ * @ingroup templates
+ */
+#}
+{% extends \"block--bare.html.twig\" %}
+";
+    }
+}
---
+++ sites/default/files/php/twig/5967c8e13add3_block--system.html.twig_KV-u21im7r2cUIvhlLAyTySHI/igt7ykKf1MkjmIF2aNbbeGNajh2puiU6kMdobuJS-pE.php
@@ -0,0 +1,78 @@
+<?php
+
+/* themes/bootstrap 7/templates/block/block--system.html.twig */
+class __TwigTemplate_fc4d5bd0d30abee76c296200521b89e9bc09a729b6df83cdf4e164e6d1a08f72 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 9
+        $this->parent = $this->loadTemplate("block--bare.html.twig", "themes/bootstrap 7/templates/block/block--system.html.twig", 9);
+        $this->blocks = array(
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "block--bare.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/bootstrap 7/templates/block/block--system.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  11 => 9,);
+    }
+
+    public function getSource()
+    {
+        return "{#
+/**
+ * @file
+ * Theme override for system blocks.
+ *
+ * @ingroup templates
+ */
+#}
+{% extends \"block--bare.html.twig\" %}
+";
+    }
+}
